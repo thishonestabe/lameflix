19072416356ad9c531ef26f754cdf99015fc54e5
thishonestabe <marquez.abraham1991@gmail.com>
2020-12-07 16:22:14 +0100

    Root reducer added

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,13 @@
+import {all} from "redux-saga/effects";
+import {combineReducers} from "redux";
+
+import * as auth from "../modules/Auth/_redux/authRedux";
+
+export const rootReducer = combineReducers({
+    auth: auth.reducer,
+
+});
+
+export function* rootSaga() {
+    yield all([auth.saga()]);
+}
